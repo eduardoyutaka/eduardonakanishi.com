a2a1bc6fe7b404b9da12cc2cdfa53761959cadaf
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2023-06-01 16:25:47 -0300

    fix: image url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
+import me from '../../public/images/me.jpg';
 
 export default function Home() {
   return (
     <main>
       <section className="py-2">
-        <Image src="/images/me.jpg" alt="me" width={200} height={200} quality={100} />
+        <Image src={me} alt="autor" width={200} height={200} quality={100} />
         <p className="my-4">
           Meu nome é Eduardo e eu sou um desenvolvedor de software desde 2016.
           No momento estou estudando <a href="https://en.wikipedia.org/wiki/Competitive_programming" className="underline underline-offset-4 text-blue-500 hover:text-blue-700 visited:text-purple-500" >programação competitiva</a> e ofereço aulas particulares de programação para todos os níveis.
